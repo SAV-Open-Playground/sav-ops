57f5731995bf7437c559b784b1818a90294ca533
MichaelYoungGo <ustb_yhb@163.com>
2023-04-26 02:24:00 +0000

    rpki build automate

--- docker_compose/host_run.sh
+++ docker_compose/host_run.sh
@@ -3,13 +3,31 @@
 FOLDER=$(cd "$(dirname "$0")";pwd)
 model_name=$1
 docker-compose down
-rm -rf ./logs/*
+rm -rf ./logs/*; mkdir ./logs
 docker rmi -f savop_bird_base
-docker build -f ./reference_router . -t savop_bird_base
+docker build -f ./dockerfiles/reference_router . -t savop_bird_base
+image_name="`grep "image: krill" compose.yml |awk '{ print $2 }'`"
+if [ "${image_name}" = "krill" ]
+then
+    docker build -f ./dockerfiles/nlnetlabs_base . -t nlnetlabs_base
+    docker build -f ./dockerfiles/krill . -t krill
+    rm -rf ./krill_data
+    rm -rf ${FOLDER}/logs/krill.log 
+    echo "" > ${FOLDER}/logs/krill.log
+    rm -rf ${FOLDER}/logs/rsync.log
+    echo "" > ${FOLDER}/logs/rsync.log
+fi
+image_name=`grep "image: routinator" compose.yml |awk '{ print $2 }'`
+if [ "${image_name}" = "routinator" ];then
+    docker build -f ./dockerfiles/routinator . -t routinator
+    rm -rf ./routinator_data
+    rm -rf ./logs/routinator.log
+    echo "" > ./logs/routinator.log
+fi
 docker container rm $(docker container ls -aq)
 # remove all stopped containers
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 # remove all images taged as <none>
 docker-compose up -d --force-recreate  --remove-orphans
 ./topo.sh
-docker-compose down
+#docker-compose down
--- rpki/routinator.sh
+++ rpki/routinator.sh
@@ -1,20 +1,25 @@
-# !/usr/bin/bash
-# set -ex
-sleep 3 # for the interface to be added
+#!/usr/bin/bash
+#set -ex
 echo '10.0.9.1        savopkrill.com' >> /etc/hosts
-
-# TODO: proced untill down finished
-# TAL_GOOD=false
-# while $TAL_GOOD
-# do
-#     RETURN_CODE=$(curl --write-out "%{http_code}\n" -k https://savopkrill:3000/testbed.tal >> /var/routinator/tals/krill.tal --output output.txt -silent)
-#     echo $RETURN_CODE
-#     if [ $RETURN_CODE -eq 200 ]; then
-#     TAL_GOOD=true;
-# done
-# update-ca-certificates
+let "TAL_GOOD=-1"
+let "MAX=60"
+mkdir /var/routinator/tals
+while [ $TAL_GOOD -ne 0 ]
+do
+   curl -k https://savopkrill.com:3000/testbed.tal > /var/routinator/tals/krill.tal
+   if [ $? -eq 0 ]; then
+	TAL_GOOD=0;
+	break;
+   fi
+   sleep 1
+   let "MAX--"
+   if [ $MAX -eq 0 ]; then
+	echo "Can't connect to krill finally! Please check out system"
+        exit -1
+   fi
+done
+#update-ca-certificates
 mkdir /var/routinator/
 mkdir /var/routinator/tals/
-curl -k https://savopkrill.com:3000/testbed.tal >> /var/routinator/tals/krill.tal
 routinator -c /etc/routinator/routinator.conf --rrdp-root-cert=/var/routinator/data/ca.pem --rrdp-root-cert=/var/routinator/data/web.pem -v -v server 
-# routinator -c /etc/routinator/routinator.conf -v -v server 
+
