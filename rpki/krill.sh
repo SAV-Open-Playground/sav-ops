cd7307c59dd9cd63059ff162762031e65d626fdc
Yuqian Shi <yuqian.shi@outlook.com>
2023-04-26 10:27:27 +0800

    add aspa to CA

--- rpki/krill.sh
+++ rpki/krill.sh
@@ -1,4 +1,6 @@
 # !/usr/bin/bash
 # set -ex
+update-ca-certificates
 rsync --daemon
-krill -c /var/krill/data/krill.conf
+python3 /var/krill/add_info.py /var/krill/roas.json /var/krill/aspas.json &
+/root/.cargo/bin/krill -c /var/krill/data/krill.conf 
