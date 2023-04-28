7d1582ce91ca01298323a98c81d8f14dd894310f
MichaelYoungGo <ustb_yhb@163.com>
2023-04-28 07:43:28 +0000

    adjust init_env.sh

--- init_env.sh
+++ init_env.sh
@@ -2,6 +2,17 @@
 set -ex
 sys_version=`cat /etc/lsb-release | grep DISTRIB_DESCRIPTION | awk -F= '{gsub("\"", ""); print $2 }'`
 # limite os version
+
+# install docker
+apt-get -y update
+# apt-get remove docker docker-engine docker.io containerd runc -y
+apt-get install ca-certificates curl gnupg -y
+sudo install -m 755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 # bird env
 apt-get -y update
 apt-get -y install build-essential flex bison autoconf ncurses-dev libreadline-dev libssh-gcrypt-dev\
@@ -13,16 +24,6 @@ pip install Flask gunicorn blinker flask-sqlalchemy netaddr -i https://pypi.mirr
 docker build -f ./dockerfiles/savop_base . -t savop_base
 curl -SL https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-# install docker
-apt-get remove docker docker-engine docker.io containerd runc -y 
-apt-get install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 #Rust env and compile 
 apt -y install curl build-essential libssl-dev openssl pkg-config git
