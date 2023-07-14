3a44b7d62467e809f156e320845f01bef2f0afed
Yuqian Shi <yuqian.shi@outlook.com>
2023-07-14 12:43:22 +0800

    add testing udp client and server

---
+++ traffic_tools/udp_server.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+-------------------------------------------------
+   File Name     UDP_server
+   Description : For traffic testing
+   Author :      yuqian.shi@outlook.com
+   date:         2023/7/14
+-------------------------------------------------
+"""
+import socket
+import time
+import sys
+if len(sys.argv)!=2:
+    print('please input listening addr( e.g. python3 udp_server.py 192.168.1.1:12345)')
+    sys.exit(-1)
+addr = sys.argv[1].split(":")
+ip = addr[0]
+port = int(addr[1])
+udpsever = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udpsever.bind((ip, port))
+print(f'listening ong {ip}:{port}')
+while True:
+    data, addr = udpsever.recvfrom(1024)
+    data=data.decode("utf-8")
+    print("from", addr, "data", data)
