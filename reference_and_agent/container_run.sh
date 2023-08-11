7d9a5a3f42dc2caa0fda147d0ff1573afaae44ba
MichaelYoungGo <ustb_yhb@163.com>
2023-08-11 02:56:01 +0000

    adjust classic_2_Strict_uRPF_2N config file format

--- reference_and_agent/container_run.sh
+++ reference_and_agent/container_run.sh
@@ -3,7 +3,7 @@
 FOLDER=$(cd "$(dirname "$0")";pwd)
 ACCESS_LOGFILE=${FOLDER}/logs/access.log
 ERROR_LOGFILE=${FOLDER}/logs/error.log
-CDDIR=${FOLDER}/extend_server/
+CDDIR=${FOLDER}/sav-agent/
 GUNICORN_COUNT=`ps aux|grep gunicorn|grep -v grep|wc -l`
 START_GUNICORN_COMMAND="gunicorn -D --chdir ${CDDIR} -b 0.0.0.0:8888 server:app --workers 1 --access-logfile ${ACCESS_LOGFILE} --error-logfile ${ERROR_LOGFILE} --log-level debug"
 if [ ${GUNICORN_COUNT} -gt 0 ];then
