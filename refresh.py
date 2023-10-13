0341b45143451595a8a07d36bb67d1239c2c5fd2
Yuqian Shi <yuqian.shi@outlook.com>
2023-10-13 09:22:13 +0800

    Create refresh.py

---
+++ refresh.py
@@ -0,0 +1,10 @@
+"""
+1. recompile bird
+2. rebuild docker images
+3. regenerate config files
+4. regenerate keys
+5. regenerate compose.yml and topo.sh for the given node and links
+"""
+
+
+import os,sys
